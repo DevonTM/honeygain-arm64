ab7a659e84fe59003833558ffe35ba41e32eaaad
DevonTM <ipangcheater@gmail.com>
2023-12-13 15:11:58 +0700

    handle box64 crash

--- root/init.sh
+++ root/init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd /app
+if [ -z "$USER" ] || [ -z "$PASS" ]; then
+    echo "Error: User or password is empty. Please set the \$USER and \$PASS variables."
+    exit 1
+fi
 
-./box64 honeygain -tou-accept -email $USER -pass $PASS -device `hostname`
+while true; do
+    BOX64_LD_LIBRARY_PATH=/app box64 /app/honeygain -tou-accept -email "$USER" -pass "$PASS" -device "$HOSTNAME"
+    echo "Box64: honeygain crashed... restarting in 30 seconds"
+    sleep 30
+done
